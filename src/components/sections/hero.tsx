a02b598af9466de5b6d56d18c275ee8f432bd0aa
Ihor Bozhuk <88133512+Jotunn1@users.noreply.github.com>
2023-11-14 13:00:59 +0200

    removed gradient bg

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -11,13 +11,13 @@ import { useSectionScroll } from "@/hooks/useSectionScroll";
 import { useActiveSectionContext } from "@/context/active-section-context";
 
 export default function Hero() {
-  const { ref } = useSectionScroll("Home", 0.5);
+  const { ref } = useSectionScroll("Hero", 0.5);
   const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
 
   return (
     <section
       ref={ref}
-      id="home"
+      id="hero"
       className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
     >
       <div className="flex items-center justify-center">
@@ -30,27 +30,26 @@ export default function Hero() {
               duration: 0.2,
             }}
           >
-            {/* Ihor TODO: replace this image with your own */}
             <Image
-              src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
-              alt="Ricardo portrait"
-              width="192"
-              height="192"
+              src="/my-photo.jpg"
+              alt="Ihor Bozhuk profile picture"
+              width="400"
+              height="400"
               quality="95"
               priority={true}
-              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+              className="h-80 w-80 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
             />
           </motion.div>
 
           <motion.span
-            className="absolute bottom-0 right-0 text-4xl"
+            className="absolute bottom-5 right-5 text-4xl"
             initial={{ opacity: 0, scale: 0 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{
               type: "spring",
               stiffness: 125,
-              delay: 0.1,
-              duration: 0.7,
+              delay: 0.2,
+              duration: 0.8,
             }}
           >
             👋
@@ -65,11 +64,11 @@ export default function Hero() {
       >
         <span className="font-bold">Hello, I'm Ihor.</span>
         <br /> I'm a front-end developer with 2 years of experience. I enjoy
-        building sites & apps. My focus is <span>React (Next.js)</span>.
+        building sites & apps. My focus is React and Next.js.
       </motion.h1>
 
       <motion.div
-        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        className="flex flex-col sm:flex-row items-center justify-center gap-4 px-4 text-lg font-medium"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{
@@ -78,23 +77,23 @@ export default function Hero() {
       >
         <Link
           href="#contact"
-          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
-          //   onClick={() => {
-          //     setActiveSection("Contact");
-          //     setTimeOfLastClick(Date.now());
-          //   }}
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-105 hover:scale-105 hover:bg-gray-950 active:scale-105 transition"
+          onClick={() => {
+            setActiveSection("Contact");
+            setTimeOfLastClick(Date.now());
+          }}
         >
-          Contact me here{" "}
+          Contact me here
           <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
         </Link>
 
         <a
-          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-105 hover:scale-105 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
           href="/CV.pdf"
           download
         >
-          Download CV{" "}
-          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+          Download CV
+          <HiDownload className="opacity-60 group-hover:translate-y-0.5 transition" />
         </a>
 
         <a
