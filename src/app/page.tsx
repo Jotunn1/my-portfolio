ded8366694d0073aa389a5a23cddedb4241f3b28
Ihor Bozhuk <88133512+Jotunn1@users.noreply.github.com>
2023-11-14 21:33:30 +0200

    added skills section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@
 
 import About from "@/components/sections/about";
 import Hero from "@/components/sections/hero";
+import Skills from "@/components/sections/skills";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <Hero />
       <About />
+      <Skills />
     </main>
   );
 }
