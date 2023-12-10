bd62c7ae5e941e2fb1d46412ca3bbd8bb071b000
Ihor Bozhuk <88133512+Jotunn1@users.noreply.github.com>
2023-12-11 00:40:23 +0200

    added contact section

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import SectionHeading from "@/components/sectionHeading";
+import SectionHeading from "@/components/section-heading";
 import { motion } from "framer-motion";
 import { useSectionScroll } from "@/hooks/useSectionScroll";
 
